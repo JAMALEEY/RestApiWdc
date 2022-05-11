f79cee9b03d89d9681b5d5a9d3d80998d4c53130
JAMALEEY <elidrissi.yassir3@gmail.com>
2022-05-11 20:02:51 +0100

    init

---
+++ src/main/java/ma/youcode/restapiwdc/RestApiWdcApplication.java
@@ -0,0 +1,13 @@
+package ma.youcode.restapiwdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWdcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiWdcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/youcode/restapiwdc/RestApiWdcApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.restapiwdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWdcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
